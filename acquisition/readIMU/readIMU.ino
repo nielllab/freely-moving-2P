46e8fcee276156e9bd4988bcd2f593b4babcaf2f
Dylan Martins <49732896+dylanmmartins@users.noreply.github.com>
2025-08-25 16:32:11 -0700

    Create readIMU.ino

---
+++ acquisition/readIMU/readIMU.ino
@@ -0,0 +1,64 @@
+// Must run on Arduino Giga R1
+// IMU should have:
+//      blue wire into SDA   (pin 20)
+//      yellow wire into SCL (pin 21)
+//      red is 3.3 V power
+//      black is Gnd
+//
+// Modified from sparkfun template 
+// https://github.com/sparkfun/SparkFun_BMI270_Arduino_Library
+// DMM, Aug 2025
+
+
+#include <Wire.h>
+#include "SparkFun_BMI270_Arduino_Library.h"
+
+BMI270 imu;
+
+uint8_t i2cAddress = BMI2_I2C_PRIM_ADDR;
+
+void setup() {
+
+    Serial.begin(115200);
+    Serial.println("BMI270 Example 1 - Basic Readings I2C");
+
+    Wire.begin();
+
+    // Check if sensor is connected and initialize
+    while(imu.beginI2C(i2cAddress) != BMI2_OK) {
+        Serial.println("Error: BMI270 not connected, check wiring and I2C address!");
+        delay(1000);
+    }
+
+    Serial.println("BMI270 connected");
+}
+
+void loop() {
+    imu.getSensorData();
+
+    // Serial.print("Acceleration in g's");
+    // Serial.print("\t");
+    // Serial.print("X: ");
+    Serial.print(imu.data.accelX, 3);
+    Serial.print(",");
+    // Serial.print("Y: ");
+    Serial.print(imu.data.accelY, 3);
+    Serial.print(",");
+    // Serial.print("Z: ");
+    Serial.print(imu.data.accelZ, 3);
+    Serial.print(",");
+
+    // Serial.print("Rotation in deg/sec");
+    // Serial.print(",");
+    // Serial.print("X: ");
+    Serial.print(imu.data.gyroX, 3);
+    Serial.print(",");
+    // Serial.print("Y: ");
+    Serial.print(imu.data.gyroY, 3);
+    Serial.print(",");
+    // Serial.print("Z: ");
+    Serial.println(imu.data.gyroZ, 3);
+
+    // Print 50x per second
+    delay(20);
+}
